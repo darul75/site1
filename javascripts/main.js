51dfaa27c41df9a68ca3642f1fa3c0dcfcd4095e
darul75 <darul75@gmail.com>
2015-03-09 17:33:40 +0100

    l

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,59 @@
-console.log('This would be the main JS file.');
+$('#submitEmail').click(function(){
+    $(window.location).attr('href', 'mailto:?subject='
+                             + encodeURIComponent("This is my subject")
+                             + "&body=" 
+                             + encodeURIComponent("This is my body")
+    );
+});
+
+$(document).ready(function() {
+  $('#fullpage').fullpage({
+    sectionsColor: ['#1bbc9b', '#4BBFC3', '#7BAABE'],
+    anchors: ['presentation', 'whoweare', 'references', 'contacts'],
+    menu: '#menu',
+    loopTop: true,
+    loopBottom: true,
+    css3: true,
+    scrollingSpeed: 800
+  });
+});
+window.sr = new scrollReveal();
+
+/*http://codepen.io/adi52i/pen/pxLou*/
+
+/*$(document).ready(function(){
+  nav = $("#menu li");
+  // Set spotlight location to default
+  item = nav.eq(0);
+
+  // Checks whether a position is saves in localStorage from previoss pages
+  if(localStorage.getItem('sItem') !== null){
+    item = nav.eq(localStorage.getItem('sItem'));
+  }
+  
+  // Create and position the spotlight
+  $("#menu").append("<div id='highlight'></div>");
+  $("#highlight").css('width', item.outerWidth()).offset({ top: item.offset().top, left: item.offset().left });
+  $("#highlight").css('height', item.outerHeight());
+});
+
+// Move spotlight according to mouse position
+$("#menu li").hover(function(){
+  // Animate to new mouse location
+  $("#highlight").stop().animate({
+    "left" : $(this).offset().left,
+    "width" : $(this).outerWidth()
+  });
+}, function(){
+  // Return to the main position
+  $("#highlight").stop().animate({
+    "left" : item.offset().left,
+    "width" : item.outerWidth()
+  });
+});*/
+
+// Save new item location
+/*$("#menu li").click(function(){
+  localStorage.setItem("sItem", $(this).index());
+  item = nav.eq($(this).index());
+});*/
